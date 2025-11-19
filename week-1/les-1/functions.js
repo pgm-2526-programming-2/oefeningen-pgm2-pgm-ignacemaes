b928581d010b180028b2fe7bfad9f5504e33619e
«pgm-ignacemaes» <ignace.maes@student.arteveldehs.be>
2025-11-19 14:43:01 +0100

    finished another small exercise

--- week-1/les-1/functions.js
+++ week-1/les-1/functions.js
@@ -5,15 +5,11 @@
 
 const name = "Ignace";
 const age = 19;
-const students = ["Tim", "Bart", "Bob"];
+const students = ["Tim", "Bart", "Bob", "Yonas", "Geert"];
 
 function logSum(a, b) {
   return a + b;
 }
-
-const division = (a, b) => {
-  return a / b;
-};
 
 const multiplier = function () {};
 
@@ -26,3 +22,49 @@ function selectRandomItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 console.log(selectRandomItem(students));
+
+function showMessage(message) {
+  console.log("Answer Checker Message:", message);
+}
+
+function checkAnswer(answer, solution, callback) {
+  if (answer === solution) {
+    callback("true");
+    return true;
+  }
+  // Geen else nodig want, indien waar stopt de functie :)
+  callback("false");
+  return false;
+}
+
+checkAnswer("ja", "nee", showMessage);
+
+// multiply, division
+// showCalculation, 3 parameters: 2 getallen en een van de bovenstaande functies
+// showCalculation print uitkomst in onze terminal => console.log gebruiken
+// sum and minus zijn gemaakt voor plezier
+
+function sum(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+function showCalculation(number1, number2, calculation) {
+  console.log(calculation(number1, number2));
+}
+
+showCalculation(6, 2, sum);
+showCalculation(6, 2, minus);
+showCalculation(6, 2, multiply);
+showCalculation(6, 2, division);
