0b3d13a5b4cd728af6270d86444999d41c0c4c52
«pgm-ignacemaes» <ignace.maes@student.arteveldehs.be>
2025-11-27 15:12:22 +0100

    Sorted object finished

--- week-2/les-1/oefeningen/sort_object.js
+++ week-2/les-1/oefeningen/sort_object.js
@@ -1,11 +1,20 @@
 let objectenArray = [
+  { naam: "Bob", leeftijd: 25 },
   { naam: "Anna", leeftijd: 30 },
   { naam: "Charlie", leeftijd: 35 },
   { naam: "Bob", leeftijd: 25 },
 ];
 
 function sorterenOpEigenschap(objecten, eigenschap) {
-  return objecten.sort();
+  return objecten.sort((a, b) => {
+    if (a[eigenschap] < b[eigenschap]) {
+      return -1;
+    }
+    if (a[eigenschap] > b[eigenschap]) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
 let gesorteerdOpNaam = sorterenOpEigenschap(objectenArray, "naam");
