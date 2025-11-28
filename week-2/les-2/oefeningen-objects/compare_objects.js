3a7a04862cce93cd65c852ce7ad563c0f167710f
«pgm-ignacemaes» <ignace.maes@student.arteveldehs.be>
2025-11-28 08:48:31 +0100

    compare_objects.js finished

---
+++ week-2/les-2/oefeningen-objects/compare_objects.js
@@ -0,0 +1,13 @@
+const objectA = { a: 1, b: 2, c: 3 };
+const objectB = { a: 1, b: 2, c: 3 };
+
+function zijnObjectenIdentiek(obj1, obj2) {
+  const stringA = JSON.stringify(obj1);
+  const stringB = JSON.stringify(obj2);
+  if (stringA === stringB) {
+    return true;
+  }
+  return false;
+}
+
+console.log(zijnObjectenIdentiek(objectA, objectB)); // true
