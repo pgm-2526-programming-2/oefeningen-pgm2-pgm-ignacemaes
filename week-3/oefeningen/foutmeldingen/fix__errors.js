81ba5cd524381a06191b145f512ea3cc8cfceb31
«pgm-ignacemaes» <ignace.maes@student.arteveldehs.be>
2025-12-07 09:19:35 +0100

    foutmeldingen oef 1 finished

---
+++ week-3/oefeningen/foutmeldingen/fix__errors.js
@@ -0,0 +1,13 @@
+// Opgave
+myVariable = "Ignace";
+console.log(myVariable);
+
+// Opgave
+x = 12;
+if (x > 10) {
+  console.log("x is groter dan 10");
+}
+
+// Opgave
+var number = 42;
+console.log(number + 2);
